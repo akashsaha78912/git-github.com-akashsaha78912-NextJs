25856ee53ee6668f23471208957ae19f13150d0c
akashsaha78912 <akashsaha78912@gmail.com>
2025-06-24 21:17:01 +0530

    Add link to blog in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link"
 export default function Home() {
     return (
-        <h1>hello world</h1>
+        <h1>hello world
+
+            <Link href="/blog">Bloggggggg</Link>
+        </h1>
     )
 }
